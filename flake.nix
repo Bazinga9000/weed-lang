2f81b1572d8dc71b3308b241c7162411a4e75ccc
Bazinga9000 <19626297+Bazinga9000@users.noreply.github.com>
2026-05-31 17:56:37 -0400

    prepare flake/cabalfile for alex/happy lexing/parsing

--- flake.nix
+++ flake.nix
@@ -4,55 +4,41 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
   };
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [ inputs.haskell-flake.flakeModule ];
 
-      perSystem = { self', pkgs, ... }: {
+      perSystem =
+        { self', pkgs, ... }:
+        {
+          haskellProjects.default = {
+            packages = {
+            };
+            settings = {
+            };
+            devShell = {
+              enable = true;
 
-        # Typically, you just want a single project named "default". But
-        # multiple projects are also possible, each using different GHC version.
-        haskellProjects.default = {
-          # The base package set representing a specific GHC version.
-          # By default, this is pkgs.haskellPackages.
-          # You may also create your own. See https://haskell.nixos.asia/package-set
-          # basePackages = pkgs.haskellPackages;
-
-          # Extra package information. See https://haskell.nixos.asia/dependency
-          #
-          # Note that local packages are automatically included in `packages`
-          # (defined by `defaults.packages` option).
-          #
-          packages = {
-            # aeson.source = "1.5.0.0";      # Override aeson to a custom version from Hackage
-            # shower.source = inputs.shower; # Override shower to a custom source path
-          };
-          settings = {
-            #  aeson = {
-            #    check = false;
-            #  };
-            #  relude = {
-            #    haddock = false;
-            #    broken = false;
-            #  };
+              tools = hp: {
+                alex = hp.alex;
+                happy = hp.happy;
+                fourmolu = hp.fourmolu;
+                cabal-install = hp.cabal-install;
+                haskell-language-server = hp.haskell-language-server;
+                hlint = hp.hlint;
+                ghcid = hp.ghcid;
+              };
+            };
           };
 
-          devShell = {
-            # Enabled by default
-            # enable = true;
-
-            # Programs you want to make available in the shell.
-            # Default programs can be disabled by setting to 'null'
-            # tools = hp: { fourmolu = hp.fourmolu; ghcid = null; };
-
-            # Check that haskell-language-server works
-            # hlsCheck.enable = true; # Requires sandbox to be disabled
-          };
+          packages.default = self'.packages.weed-lang;
         };
-
-        # haskell-flake doesn't set the default package, but you can do it here.
-        packages.default = self'.packages.weed-lang;
-      };
     };
 }
