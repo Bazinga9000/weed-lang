78beb652e1198b87b12aedc1805ac8bd84103bae
Bazinga9000 <19626297+Bazinga9000@users.noreply.github.com>
2026-06-03 22:50:51 -0400

    main: basic repl

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,34 @@
 module Main where
 
+import Control.Monad (forever)
+import Evaluator
+import Evaluator.Types (displayError, displayObservable)
+import Parser.Lexer
+import Parser.Lowerer
+import Parser.SurfaceParser
+import System.Exit (exitSuccess)
+import System.IO (hFlush, stdout)
+import TypeChecker
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = forever $ do
+  putStr "weed> "
+  hFlush stdout
+  input <- getLine
+  if input == "exit" then exitSuccess else repl input
+
+repl :: String -> IO ()
+repl input = do
+  let toks = scanTokens input
+  let surface = parseSurface toks
+  let coreu = case lower surface of
+        (Right cue) -> cue
+        (Left e) -> error $ show e
+  let coret = case typeCheck coreu of
+        (Right cte) -> cte
+        (Left e) -> error $ show e
+
+  ev <- evaluate coret
+  case ev of
+    (Right v) -> putStrLn $ displayObservable v
+    (Left err) -> error $ displayError err
