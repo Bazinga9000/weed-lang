b71be6eea17f5a86bed92f3d95dfa8789ad03682
Bazinga9000 <19626297+Bazinga9000@users.noreply.github.com>
2026-06-04 19:31:39 -0400

    repl: use haskeline for history

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,26 @@
 module Main where
 
-import Control.Monad (forever)
+import Control.Monad.IO.Class (liftIO)
+import System.Console.Haskeline
 import Evaluator
 import Evaluator.Types (displayError, displayObservable)
 import Parser.Lexer
 import Parser.Lowerer
 import Parser.SurfaceParser
-import System.Exit (exitSuccess)
-import System.IO (hFlush, stdout)
 import TypeChecker
 
 main :: IO ()
-main = forever $ do
-  putStr "weed> "
-  hFlush stdout
-  input <- getLine
-  if input == "exit" then exitSuccess else repl input
+main = runInputT defaultSettings loop
+  where
+    loop :: InputT IO ()
+    loop = do
+      minput <- getInputLine "weed> "
+      case minput of
+        Nothing -> return ()
+        Just "exit" -> return ()
+        Just input -> do
+          liftIO $ repl input
+          loop
 
 repl :: String -> IO ()
 repl input = do
